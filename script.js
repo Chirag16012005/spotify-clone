0b7ca802fb168ae5077562f794825e60f043cd87
Chirag16012005 <202301259@daiict.ac.in>
2025-06-11 15:51:01 +0530

    Finally Added some javascript

---
+++ script.js
@@ -0,0 +1,40 @@
+async function getsongs()
+{
+    let a=await fetch("http://127.0.0.1:5500/songs/");
+
+    let response=await a.text();
+    let div=document.createElement("div");
+    div.innerHTML=response;
+    let as=div.getElementsByTagName("a");
+    let songs=[];
+    for(let i=0;i<as.length;i++)
+    {
+        const element=as[i];
+
+        if(element.href.endsWith(".mp3"))
+        {
+            songs.push(element.href);
+        }
+    }
+    return songs;
+
+    
+}
+async function main(){
+    //get the list of all songs from the API
+    let songs=await getsongs();
+    console.log(songs);
+
+    let songul=document.querySelector(".songlist").getElementsByTagName("ul")[0];
+
+    for(const song of songs)
+    {
+       songul.innerHTML+=`<li><a href="${song}">${song.split("/").pop()}</a></li>`;
+    }
+    //play the songs or do something with them
+    var audio = new Audio(songs[0]);
+    audio.play(); 
+
+
+}
+main();
